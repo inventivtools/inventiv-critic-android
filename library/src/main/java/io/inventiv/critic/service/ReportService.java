42f5d322f4bcec649e2d7c444317ea74a3f44668
Dave Lane <dclane@gmail.com>
2018-02-08 16:05:34 -0600

    Making a simpler way to create reports from the Android client.

--- library/src/main/java/io/inventiv/critic/service/ReportService.java
+++ library/src/main/java/io/inventiv/critic/service/ReportService.java
@@ -3,16 +3,18 @@ package io.inventiv.critic.service;
 import java.util.List;
 
 import io.inventiv.critic.client.Configuration;
-import io.inventiv.critic.model.NewReportRequest;
 import io.inventiv.critic.model.Report;
+import okhttp3.MultipartBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
@@ -40,11 +42,11 @@ public interface ReportService {
     // TODO move these static headers to a request interceptor.
     @Headers({
         "Accept: application/json",
-        "Content-Type: application/json",
         "User-Agent: " + Configuration.USER_AGENT
     })
+    @Multipart
     @POST("/api/v1/reports/")
-    Call<Report> create(@Body NewReportRequest.Wrapper report);
+    Call<Report> create(@Part List<MultipartBody.Part> parts);
 
     // TODO move these static headers to a request interceptor.
     @Headers({
